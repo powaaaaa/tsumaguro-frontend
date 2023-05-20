5725da19ad31b0216a387c2301801a3a4273c4b2
semi-koron <6jimoto6@gmail.com>
2023-05-20 14:57:49 +0900

    ホーム画面を編集

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
 import Link from "next/link";
+import Container from "@mui/material";
+import Modal from "react-modal";
+import HelpIcon from '@mui/icons-material/Help';
+import CookieIcon from '@mui/icons-material/Cookie';
+import SensorDoorOutlinedIcon from '@mui/icons-material/SensorDoorOutlined';
+import Button from '@mui/material/Button';
 
 function HomePage() {
   const onClike = () => {
@@ -8,19 +14,41 @@ function HomePage() {
     alert(str);
     return;
   };
+
   return (
+
     <div>
-      <h1>Home Page</h1>
-      <button type="button" onClick={onClike}>
-        cookie
-      </button>
-      <div>ルール説明とルーム作成</div>
-      <br />
-      <Link href="/Setting" className="border-solid border-2 rounded-md">
+
+      <h1 style={{ fontSize: '50px' }}>Home Page</h1>
+
+      <div className="h-96" style={{fontSize: 70,display:'flex',justifyContent:'center', alignItems: 'center'}}>
+        ここにタイトルを入力
+
+      </div>
+
+      <div className="h-16" style={{display: 'flex', justifyContent: 'center', alignItems: 'center' , gap: '20px' }}>
+
+      <div>
+      <Button variant="contained" className="hover: text-black" style={{backgroundColor:'Gainsboro'}} >
+        <HelpIcon sx={{ fontSize: 40 }}/>
+        遊び方
+      </Button>
+      </div>
+      
+      <div>
+      <Button variant="contained" className="hover: text-black" style={{backgroundColor:'Gainsboro'}} >
+        <Link href="/Setting">
+        <SensorDoorOutlinedIcon sx={{ fontSize: 40 }} />
         ルームを作る(/Settingへ)
-      </Link>
+        </Link>
+      </Button>
+      </div>
+      </div>
+
+
     </div>
   );
 }
+
 
 export default HomePage;

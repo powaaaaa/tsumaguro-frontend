8372e45a3af5a3f1a0b194999b330083829090df
semi-koron <6jimoto6@gmail.com>
2023-05-20 20:59:03 +0900

    settingにホームに戻るボタンを追加

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,4 +1,14 @@
 import Link from "next/link";
+import * as React from 'react';
+import Container from "@mui/material";
+import HelpIcon from '@mui/icons-material/Help';
+import CookieIcon from '@mui/icons-material/Cookie';
+import ReplyIcon from '@mui/icons-material/Reply';
+import SensorDoorOutlinedIcon from '@mui/icons-material/SensorDoorOutlined';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 function SettingPage() {
   // room_idを取得
@@ -6,7 +16,10 @@ function SettingPage() {
 
   return (
     <div>
-      <h2>Setting Page</h2>
+    <div>
+      <h2 style={{ fontSize: '50px' }}>
+        Setting Page
+      </h2>
       <div>
         ルーム作成
         <br />
@@ -15,6 +28,19 @@ function SettingPage() {
 
       <Link href={`/Room/${room_id}`}>ルーム入室</Link>
     </div>
+
+    <div className="abusolute h-72"></div>
+
+<div>
+  <Button variant="contained" style={{backgroundColor:'Gainsboro'}}  className="absolute left-10 bottom-10  text-black">
+<Link href={"http://localhost:3000/"}>
+  <ReplyIcon sx={{ fontSize: 40 }}  />
+ホーム画面に戻る
+</Link>
+  </Button>
+</div>
+
+</div>
   );
 }
 

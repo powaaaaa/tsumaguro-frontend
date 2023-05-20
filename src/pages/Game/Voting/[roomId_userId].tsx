7fb86216fa471bb38025de03013ca8c9b50ee874
semi-koron <6jimoto6@gmail.com>
2023-05-21 04:06:15 +0900

    選択肢の追加

--- src/pages/Game/Voting/[roomId_userId].tsx
+++ src/pages/Game/Voting/[roomId_userId].tsx
@@ -1,8 +1,61 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { IdType, postCookie } from "@/pages/axios";
+import * as React from 'react';
+import { Theme, useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  '(A)',
+  '(B)',
+  '(C)',
+  '(D)',
+  '(E)'
+];
+
+function getStyles(name: string, personName: string, theme: Theme) {
+  return {
+    fontWeight: personName === name ? theme.typography.fontWeightMedium : theme.typography.fontWeightRegular,
+  };
+}
+
+const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+  const {
+    target: { value },
+  } = event;
+  setPersonName(
+    // On autofill we get a stringified value.
+    typeof value === 'string' ? value.split(',') : value,
+  );
+};
 
 function PlayingPage() {
+
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string>('');
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const selectedName = event.target.value;
+    setPersonName(selectedName);
+    // 選択された選択肢を変数に代入する
+    // ここで必要な処理を行う
+    console.log('選択された名前:', selectedName);
+  };
+
   const router = useRouter();
   const roomId = router.query.roomId_userId;
 
@@ -84,12 +137,41 @@ function PlayingPage() {
 
   return (
     <div>
-      <h1>インサイダー投票(動的生成)[roomId: {roomId}]</h1>
+      <h1 style={{ fontSize: "50px", }}>Voting(動的生成)[roomId: {roomId}]</h1>
+      <div className="absolute left-28" style={{backgroundColor:'Silver'}}>
+        ~インサイダーを投票~
+      </div>
+      <div  className="h-72" style={{backgroundColor:'Silver',display: 'flex', justifyContent: 'center', alignItems: 'center' , gap: '20px' }}>
+      <div>
 
-      <div>誰がインサイダーか投票(お題を間違えた場合、表示されない)</div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-name-label">Name</InputLabel>
+        <Select
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          multiple={false}
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput label="Name" />}
+          MenuProps={MenuProps}
+        >
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, personName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      </div>
       <button type="button" onClick={indexCustom}>
         インサイダー投票結果へ
       </button>
+      </div>
     </div>
   );
 }

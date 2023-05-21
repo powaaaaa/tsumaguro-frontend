757da4cc95c15ed18bfcf94a3f6e1774df80f9a5
semi-koron <6jimoto6@gmail.com>
2023-05-21 14:04:25 +0900

    修正

--- src/pages/Game/Voting_result/[roomId_userId].tsx
+++ src/pages/Game/Voting_result/[roomId_userId].tsx
@@ -3,6 +3,30 @@ import Link from "next/link";
 import { IdType, postCookie } from "@/pages/axios";
 import Button from '@mui/material/Button';
 import ScoreboardIcon from '@mui/icons-material/Scoreboard';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  UserName: string,
+  Id: number,
+  host: string
+) {
+  return { UserName,Id,host};
+}
+
+const rows = [
+  createData('a',111,'y'),
+  createData('b',112,'n'),
+  createData('c',121,'n'),
+  createData('d',122,'n'),
+  createData('e',211,'n'),
+];
 
 
 function RoomDetail() {
